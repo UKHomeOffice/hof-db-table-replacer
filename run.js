722d687c55c059c9ee05d5a6226b44efbfdf4e16
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-03-14 10:28:53 +0000

    IMB-105 Add filevault utils in lib

--- run.js
+++ run.js
@@ -6,12 +6,39 @@ const client = require(`./db/${config.db.client}`);
 const Model = require(`./db/models/${config.db.model}`);
 const db = new Model();
 
-function runUpdate() {
+const fv = require('./lib/file-vault-utils');
+
+const axios = require('axios');
+// const { Readable } = require('stream');
+const { Readable } = require('node:stream');
+
+async function runUpdate() {
   logger.log('info', `Preparing table update for ${config.service.serviceName}`);
 
-  db.getLatestUrl(client)
-    .then(url => logger.log('info', `Latest uploaded file is at: ${url}`))
-    .catch(error => logger.log('error', 'collecting latest file URL:', error));
+  try {
+    const dataFileUrl = await db.getLatestUrl(client);
+    const fvToken = await fv.auth();
+    // const dataFile = await fv.getFile(dataFileUrl, fvToken);
+
+    const stream = new Readable();
+    const response = await axios(fv.getFileConfig(dataFileUrl, fvToken));
+    response.data.pipe(stream);
+
+    let counter = 0;
+    stream.on('data', chunk => {
+      counter++;
+      console.log(`CHUNK ${counter}`, chunk.toString());
+    })
+
+
+
+    stream.on('end', () => {
+      logger.log('info', 'complete!')
+    });
+
+  } catch (error) {
+    logger.log('error', 'error:', error);
+  }
 }
 
 runUpdate();

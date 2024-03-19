45f83cfce44529ac78b7a170d34eb468ea8e40a5
Jamie Carter <137879919+jamiecarterHO@users.noreply.github.com>
2024-03-19 13:38:19 +0000

    IMB-105: Auth with Keycloak and make request to File-vault to obtain latest CSV data (#5)
    * IMB-105 Add filevault utils in lib
    
    * IMB-105 Stream CSV into csv-parse
    
    * IMB-105 update logical order of runUpdate function
    
    * Improve logging for end of ticket testing
    
    * IMB-105 Add tests for file vault utils, fix linting
    
    * IMB-106 add memory usage monitoring

--- run.js
+++ run.js
@@ -1,17 +1,74 @@
+/* eslint-disable no-cond-assign */
 const config = require('./config');
 
+const axios = require('axios');
+const { parse } = require('csv-parse');
+
 const logger = require('./lib/logger')({ env: config.env });
+const fv = require('./lib/file-vault-utils');
 
 const client = require(`./db/${config.db.client}`);
 const Model = require(`./db/models/${config.db.model}`);
 const db = new Model();
 
-function runUpdate() {
+async function runUpdate() {
   logger.log('info', `Preparing table update for ${config.service.serviceName}`);
 
-  db.getLatestUrl(client)
-    .then(url => logger.log('info', `Latest uploaded file is at: ${url}`))
-    .catch(error => logger.log('error', 'collecting latest file URL:', error));
+  try {
+    // Log memory usage over time.
+    // TODO Remove this before prod
+    setInterval(() => {
+      logger.log('info', `Used: ${process.memoryUsage().heapUsed / 1024 / 1024}`);
+    }, 50);
+
+    // Get the most recent CSV data URL from RDS
+    const dataFileUrl = await db.getLatestUrl(client);
+
+    // Authenticate with Keycloak and receive token
+    const fvToken = await fv.auth();
+
+    // Get the data file as a stream using URL and token
+    const response = await axios(fv.fileRequestConfig(dataFileUrl, fvToken));
+    const axiosStream = response.data;
+
+    // Setup CSV parser
+    const records = [];
+    const parser = parse({ from: 2, trim: true, columns: ['cepr', 'dob', 'dtr']});
+
+    axiosStream.on('error', error => {
+      logger.log('error', 'Axios stream error: ', error.message);
+      throw error;
+    });
+
+    axiosStream.on('end', () => {
+      parser.end();
+    });
+
+    parser.on('readable', () => {
+      let record;
+      while ((record = parser.read()) !== null) {
+        // TODO validate
+        records.push(record);
+      }
+      // It may also be possble to batch insert from here in chunks rather than load all records into memory.
+    });
+
+    parser.on('error', error => {
+      logger.log('error', 'CSV parsing error: ', error.message);
+      throw error;
+    });
+
+    parser.on('end', async () => {
+      // eslint-disable-next-line no-console
+      console.log(records);
+      logger.log('info', `Records parsed from rows ${records[0].cepr} to ${records[records.length - 1].cepr}`);
+    });
+
+    // Start streaming data from Axios into CSV parser
+    axiosStream.pipe(parser);
+  } catch (error) {
+    logger.log('error', 'error:', error);
+  }
 }
 
 runUpdate();
---
+++ test/unit/file-vault-utils.test.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+const fv = require('../../lib/file-vault-utils');
+
+const fakeUrl = 'https://...';
+const fakeToken = '1234567890';
+
+jest.mock('axios');
+jest.mock('../../config.js', () => {
+  const originalModule = jest.requireActual('../../config.js');
+  return {
+    ...originalModule,
+    keycloak: { tokenUrl: fakeUrl }
+  };
+});
+
+describe('The auth() function', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('succeeding', () => {
+    beforeAll(() => {
+      const resp = {data: {access_token: fakeToken}};
+      axios.mockResolvedValue(resp);
+    });
+    test('should fetch token and return in correct format', () => {
+      return fv.auth().then(data => expect(data).toEqual({ bearer: '1234567890' }));
+    });
+  });
+
+  describe('failing', () => {
+    beforeAll(() => {
+      axios.mockRejectedValue(new Error('A general error'));
+    });
+    test('should return an error type.', () => {
+      return fv.auth().catch(error => {
+        expect(error.message).toBeDefined();
+      });
+    });
+  });
+
+  describe('with no keycloak URL available', () => {
+    beforeAll(() => {
+      jest.unmock('../../config.js');
+      axios.mockResolvedValue({data: {}});
+    });
+
+    test('Should throw a tokenUrlUndefined error.', () => {
+      return fv.auth().catch(error => {
+        expect(error.message).toEqual('tokenUrlUndefined');
+      });
+    });
+  });
+});
+
+describe('The fileRequestConfig() function', () => {
+  const bearer = { bearer: fakeToken };
+  const config = fv.fileRequestConfig(fakeUrl, bearer);
+
+  test('should return an object type', () => {
+    expect(typeof config === 'object').toBe(true);
+  });
+
+  test('returned object should have all necessary properties', () => {
+    expect(config).toHaveProperty('url', fakeUrl);
+    expect(config).toHaveProperty('method', 'get');
+    expect(config).toHaveProperty('responseType', 'stream');
+    expect(config).toHaveProperty('headers.Authorization', `Basic ${fakeToken}`);
+  });
+});
